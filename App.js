813cf867273a88bbdb1647b70cd9517021c31049
HanJul_01 <129282275+Hanjuri@users.noreply.github.com>
2024-07-04 18:05:07 +0900

    refactor:깃확인

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your pp!</Text>
+      <Text>Open up App.js tto start working on your pp!</Text>
       <StatusBar style="auto" />
     </View>
   );
