2b6f079f8cc54116315ef5ea51a454806cef29b1
sayyyho <323psh@naver.com>
2024-07-13 20:32:59 +0900

    Feat: 공통 헤더 구현

--- src/pages/home/home.main.jsx
+++ src/pages/home/home.main.jsx
@@ -1,39 +1,24 @@
-// src/components/home.main.jsx
-import * as React from 'react';
-import { View, StyleSheet, ScrollView} from 'react-native';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import RecordScreen from '../record/record.main';
-import StatisticScreen from '../statistics/statistics.main';
-import HomeToday from '../home/home.today';
-import HomeActivity from './home.activity';
-
-const TopTab = createMaterialTopTabNavigator();
-
-function HomeTabs() {
-  return (
-    <TopTab.Navigator>
-      <TopTab.Screen name="Record" component={RecordScreen} />
-      <TopTab.Screen name="Statistics" component={StatisticScreen} />
-    </TopTab.Navigator>
-  );
-}
+import * as React from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
+import HomeToday from "./home.today";
+import { HeaderView } from "../../components/Header";
 
 const HomeScreen = () => {
   return (
-    <ScrollView style = {styles.wrapper}>
-      <HomeToday/>
-      <HomeActivity></HomeActivity>
-    </ScrollView>
+    <View style={{ flex: 1 }}>
+      <ScrollView style={styles.wrapper}>
+        <HomeToday />
+        {/* <HomeActivity /> */}
+      </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   wrapper: {
-    flex:1,
-    width: '100%',
+    flex: 1,
+    width: "100%",
   },
 });
-
 
 export default HomeScreen;
