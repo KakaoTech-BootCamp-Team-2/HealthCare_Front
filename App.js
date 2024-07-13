87f7cfa8cefdec3346b2cb294900288ea2f0d5c5
Seho Park <34148750+sayyyho@users.noreply.github.com>
2024-07-13 20:35:52 +0900

    Feat: 공통 헤더 구현 (#15)

--- App.js
+++ App.js
@@ -1,27 +1,64 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './src/pages/home/home.main';
-import MyPageScreen from './src/pages/mypage/mypage.main';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { enableScreens } from 'react-native-screens';
-
-
-enableScreens();
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import HomeScreen from "./src/pages/home/home.main";
+import MyPageScreen from "./src/pages/mypage/mypage.main";
+import StatisticsScreen from "./src/pages/statistics/statistics.main";
+import { createStackNavigator } from "@react-navigation/stack";
+import { HeaderView } from "./src/components/Header";
 
 const BottomTab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const HomeStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="HomeMain"
+      component={HomeScreen}
+      options={{ headerShown: false }} // 헤더 숨김
+    />
+    <Stack.Screen
+      name="Statistics"
+      component={StatisticsScreen}
+      options={{ headerShown: false }} // 헤더 숨김
+    />
+  </Stack.Navigator>
+);
+
+const MyPageStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="MyPageMain"
+      component={MyPageScreen}
+      options={{ headerShown: false }} // 헤더 숨김
+    />
+  </Stack.Navigator>
+);
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <BottomTab.Navigator>
-          <BottomTab.Screen name="Home" component={HomeScreen} />
-          <BottomTab.Screen name="My" component={MyPageScreen} />
+        <BottomTab.Navigator
+          screenOptions={{
+            tabBarLabelStyle: { fontSize: 12 },
+            tabBarItemStyle: { width: 100 },
+            tabBarStyle: { backgroundColor: "powderblue" },
+          }}
+        >
+          <BottomTab.Screen
+            name="Home"
+            component={HomeStack}
+            options={{ header: () => <HeaderView /> }} // HomeScreen에서 별도로 헤더를 설정
+          />
+          <BottomTab.Screen
+            name="MyPage"
+            component={MyPageStack}
+            options={{ headerShown: false }} // 헤더 숨김
+          />
         </BottomTab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   );
 }
-
-
---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import * as S from "./Header.styled";
+
+export const HeaderView = () => {
+  const navigation = useNavigation();
+
+  return (
+    <S.Wrapper>
+      <Text
+        onPress={() => {
+          navigation.navigate("HomeMain"); // HomeStack 내 HomeMain으로 이동
+        }}
+      >
+        메인
+      </Text>
+      <Text
+        onPress={() => {
+          navigation.navigate("Statistics"); // HomeStack 내 Statistics으로 이동
+        }}
+      >
+        통계
+      </Text>
+      <Text
+        onPress={() => {
+          navigation.navigate("MyPage"); // MyPageStack 내 MyPageMain으로 이동
+        }}
+      >
+        마이페이지
+      </Text>
+    </S.Wrapper>
+  );
+};
